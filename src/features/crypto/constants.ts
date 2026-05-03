d2c8a69ad62102d29fa60680237949d2f449a1a2
Nipaaaa1 <frostone224@gmail.com>
2026-05-03 04:27:11 +0000

    refactor: split index.astro to several components

---
+++ src/features/crypto/constants.ts
@@ -0,0 +1,5 @@
+export const currencies = [
+  { code: 'USD', symbol: '$', price: '95,420.00', marketCap: '1.88T', volume: '42.5B' },
+  { code: 'IDR', symbol: 'Rp', price: '1,512,450,000', marketCap: '29.7P', volume: '672.3T' },
+  { code: 'EUR', symbol: '€', price: '88,150.00', marketCap: '1.74T', volume: '39.2B' },
+];
